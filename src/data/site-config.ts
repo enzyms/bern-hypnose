2a9c509bec333264c09036ed0f4a4a61af7f66ee
Sylvain Aerni <sylvainaerni@macbook-pro.home>
2024-02-11 19:00:56 +0100

    inital commit

---
+++ src/data/site-config.ts
@@ -0,0 +1,129 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+export type Subscribe = {
+    title?: string;
+    text?: string;
+    formUrl: string;
+};
+
+export type SiteConfig = {
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    subscribe?: Subscribe;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    title: 'Coaching & Hypnotherapie',
+    subtitle: 'Janine Aerni',
+    description: 'Coaching & Hypnotherapie in Bern',
+    image: {
+        src: '/dante-preview.jpg',
+        alt: 'Dante - Astro.js and Tailwind CSS theme'
+    },
+    headerNavLinks: [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        {
+            text: 'Coaching',
+            href: '/coaching'
+        },
+        {
+            text: 'Hypnosetherapie',
+            href: '/hypnosetherapie'
+        },
+        {
+            text: 'Blog',
+            href: '/blog'
+        },
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        }
+    ],
+    footerNavLinks: [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        {
+            text: 'Coaching',
+            href: '/coaching'
+        },
+        {
+            text: 'Hypnosetherapie',
+            href: '/hypnosetherapie'
+        },
+        {
+            text: 'Blog',
+            href: '/blog'
+        },
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        },
+    ],
+    socialLinks: [
+        {
+            text: 'LinkedIn',
+            href: 'https://www.linkedin.com/'
+        }
+    ],
+    hero: {
+        title: 'Coaching & Hypnosetherapie',
+        text: "Janine Aerni, introduction here",
+        actions: [
+            {
+                text: 'Get in Touch',
+                href: '/contact'
+            },
+            {
+                text: 'About me',
+                href: '/about'
+            }
+        ]
+    },
+    subscribe: {
+        title: 'Subscribe to Dante Newsletter',
+        text: 'One update per week. All the latest posts directly in your inbox.',
+        formUrl: '#'
+    },
+    postsPerPage: 8,
+    projectsPerPage: 8
+};
+
+export default siteConfig;
