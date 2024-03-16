ad28523e9b6075f28f795c04210005a6c559d189
Sylvain Aerni <sylvainaerni@macbook-pro.home>
2024-03-16 16:03:10 +0100

    Fix mobile navigation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,16 @@ module.exports = {
             sans: ['Satoshi', ...defaultTheme.fontFamily.sans],
         },
         extend: {
+            screens: {
+                'xs': '480px',
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+                '2xl': '1536px',
+                '3xl': '1980px',
+                '4xl': '2800px',
+            },
             textColor: {
                 main: 'rgb(var(--color-text-main) / <alpha-value>)'
             },
